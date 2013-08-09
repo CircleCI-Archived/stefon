e480ef6c923722a4fd2419d404ae9c900deacec5
Paul Biggar <paul.biggar@gmail.com>
2013-08-09 00:55:03 -0700

    Minor project.clj cleanup. Change version #

--- stefon-core/project.clj
+++ stefon-core/project.clj
@@ -1,15 +1,14 @@
-(defproject stefon "0.4.0"
+(defproject stefon "0.5.0-SNAPSHOT"
   :description "Asset pipeline ring middleware"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :url "http://github.com/edgecase/stefon"
+  :url "http://github.com/circleci/stefon"
   :dependencies [[ring/ring-core "1.1.8"]
                  [clj-time "0.4.4"]
                  [org.clojure/core.incubator "0.1.1"]
                  [com.google.javascript/closure-compiler "r1592"]
                  [clj-v8 "0.1.4"]
-                 [clj-v8-native "0.1.4"]
-                 [org.mozilla/rhino "1.7R4"]
-                 [org.clojure/clojure "1.4.0"]]
-  :dev-dependencies [[org.clojure/clojure "1.5.0"]]
-  :profiles {:dev {:dependencies [[ring-mock "0.1.4"]]}})
+                 [clj-v8-native "0.1.4"]]
+  :profiles {:dev {:dependencies [[ring-mock "0.1.4"]
+                                  [org.clojure/clojure "1.5.0"]
+                                  [bond "0.2.5" :exclusions [org.clojure/clojure]]]}})
