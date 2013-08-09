3065411c0a2796f31f0402ed221b5d041f3a21b3
Paul Biggar <paul.biggar@gmail.com>
2013-08-08 19:41:55 -0700

    Refactor and fix digest

--- stefon-core/src/stefon/digest.clj
+++ stefon-core/src/stefon/digest.clj
@@ -1,12 +1,25 @@
 (ns stefon.digest
+  (:require [stefon.util :refer (inspect)])
   (:import [java.security MessageDigest]))
 
+(derive (class (make-array Byte/TYPE 0)) ::bytes)
+(derive java.lang.String ::string-like)
+(derive java.lang.StringBuilder ::string-like)
+
 (defn md5 [bytes]
-  (.digest (MessageDigest/getInstance "MD5") bytes))
+  (-> "MD5"
+      MessageDigest/getInstance
+      (.digest bytes)))
 
 (defmulti digest class)
 (defmethod digest ::bytes [bytes]
-  (format "%032x" (BigInteger. 1 (md5 bytes))))
+  (->> bytes
+       md5
+       (BigInteger. 1)
+       (format "%032x")))
 
 (defmethod digest ::string-like [string]
-  (md5 (.getBytes (str string) "UTF-8")))
+  (-> string
+      str
+      (.getBytes "UTF-8")
+      digest))
