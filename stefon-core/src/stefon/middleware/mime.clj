86d3f621afdcde82fc67cb685b8f65a2743e8ad3
Paul Biggar <paul.biggar@gmail.com>
2013-08-10 11:09:04 -0700

    Fix stefan mime-types - we use a different pattern now

--- stefon-core/src/stefon/middleware/mime.clj
+++ stefon-core/src/stefon/middleware/mime.clj
@@ -1,11 +1,13 @@
 (ns stefon.middleware.mime
   "Middleware for sending mime types of stefon files"
-  (:require [ring.util.response :as response]))
+  (:require [ring.util.response :as response]
+            [stefon.util :refer (inspect)]))
 
 (defn stefon-file-type [filename]
+  (inspect filename)
   (cond
-   (re-matches #".*css-[\da-f]{32}\.stefon$" filename) "text/css"
-   (re-matches #".*js-[\da-f]{32}\.stefon$" filename) "text/javascript"))
+   (re-matches #".*css.stefon$" filename) "text/css"
+   (re-matches #".*js.stefon$" filename) "text/javascript"))
 
 (defn wrap-stefon-mime-types
   [app]
@@ -15,7 +17,6 @@
         (let [filename (.getPath body)
               file-type (stefon-file-type filename)]
           (if file-type
-            (-> response
-                (response/content-type file-type))
+            (response/content-type response file-type)
             response))
         response))))
