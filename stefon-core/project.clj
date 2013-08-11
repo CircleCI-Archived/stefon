a85ad4e335de5f7aed5a62cb7dedd09de7e7f923
Paul Biggar <paul.biggar@gmail.com>
2013-08-11 10:24:28 -0700

    Add missing dependency: clojure.tools.logging

--- stefon-core/project.clj
+++ stefon-core/project.clj
@@ -6,6 +6,7 @@
   :dependencies [[ring/ring-core "1.1.8"]
                  [clj-time "0.4.4"]
                  [org.clojure/core.incubator "0.1.1"]
+                 [org.clojure/tools.logging "0.2.3"]
                  [com.google.javascript/closure-compiler "r1592"]
                  [clj-v8 "0.1.4"]
                  [clj-v8-native "0.1.4"]]
