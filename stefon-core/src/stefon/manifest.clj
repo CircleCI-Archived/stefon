fd2083abb4ce41220db9fb690896c1dfb445d5d0
Paul Biggar <paul.biggar@gmail.com>
2013-08-11 16:48:38 -0700

    Replace cache with a manifest, which is what it was really trying to be

---
+++ stefon-core/src/stefon/manifest.clj
@@ -0,0 +1,33 @@
+(ns stefon.manifest
+ "Maps adrfs to the compiled form. Can be saved from or loaded to a manifest-file"
+  (:require [stefon.settings :as settings]
+            [stefon.path :as path]
+            [stefon.util :refer (dump)]
+            [cheshire.core :as json]))
+
+(defonce mapping (atom {}))
+
+(defn set!
+  [undigested digested]
+  {:pre [(path/asset-uri? undigested)
+         (path/asset-uri? digested)
+         (path/digest-path? digested)
+         (not (path/digest-path? digested))]}
+  (swap! mapping assoc undigested digested))
+
+(defn fetch [adrf]
+  (get @mapping adrf))
+
+(defn clear! []
+  (reset! mapping {}))
+
+(defn save! []
+  (-> @mapping
+      (#(json/generate-string % {:pretty true}))
+      (spit (settings/manifest-file))))
+
+(defn load! []
+  (->> (settings/manifest-file)
+       slurp
+       json/parse-string
+       (swap! mapping identity)))
