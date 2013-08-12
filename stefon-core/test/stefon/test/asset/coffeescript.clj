84569c5d5c3e6c75a554f80edf8464621307127c
Paul Biggar <paul.biggar@gmail.com>
2013-08-11 18:45:52 -0700

    Refactor test functinos into helpers

--- stefon-core/test/stefon/test/asset/coffeescript.clj
+++ stefon-core/test/stefon/test/asset/coffeescript.clj
@@ -1,33 +1,16 @@
 (ns stefon.test.asset.coffeescript
-  (:require [stefon.asset :as asset]
+  (:require [stefon.test.helpers :as h]
             [stefon.asset.coffeescript]
-            [stefon.settings :as settings]
-            [stefon.util :refer (dump)]
-            [stefon.test.helpers :as h])
+            [stefon.util :refer (dump)])
   (:use clojure.test))
-
-(defn test-expected [root file expected-file expected]
-  (settings/with-options {:asset-roots [root]}
-    (let [[result-file content] (-> file asset/compile)]
-      (is (= expected content))
-      (is (= expected-file result-file)))))
-
-(defn test-syntax [root file expecteds]
-  (settings/with-options {:asset-roots [root]}
-    (try
-      (asset/compile file)
-      (is false)
-      (catch Exception e
-        (doseq [expected expecteds]
-          (is (h/has-text? (.toString e) expected)))))))
 
 
 (deftest test-coffeescript
-  (test-expected "test/fixtures/assets"
-                 "javascripts/test.js.coffee"
-                 "javascripts/test.js"
-                 "(function() {\n\n  (function(param) {\n    return alert(\"x\");\n  });\n\n}).call(this);\n")
+  (h/test-expected "test/fixtures/assets"
+                   "javascripts/test.js.coffee"
+                   "javascripts/test.js"
+                   "(function() {\n\n  (function(param) {\n    return alert(\"x\");\n  });\n\n}).call(this);\n")
 
-  (test-syntax "test/fixtures/assets"
-               "javascripts/bad.js.coffee"
-               ["on line 2"  "unmatched ]"]))
+  (h/test-syntax "test/fixtures/assets"
+                 "javascripts/bad.js.coffee"
+                 ["on line 2"  "unmatched ]"]))
--- stefon-core/test/stefon/test/helpers.clj
+++ stefon-core/test/stefon/test/helpers.clj
@@ -1,6 +1,9 @@
 (ns stefon.test.helpers
   (:require [stefon.settings :as settings]
-            [clojure.java.io :as io]))
+            [stefon.asset :as asset]
+            [stefon.util :refer (dump)]
+            [clojure.java.io :as io]
+            [clojure.test :refer (is)]))
 
 (defn contains-file? [seq filename]
   (->> seq
@@ -15,9 +18,27 @@
        count
        pos?))
 
+
 (defn has-text?
   "returns true if expected occurs in text exactly n times (one or more times if not specified)"
   ([text expected]
      (not= -1 (.indexOf text expected)))
   ([text expected times]
      (= times (count (re-seq (re-pattern expected) text)))))
+
+(defn test-expected [root file expected-file expected]
+  (settings/with-options {:asset-roots [root]}
+    (let [[result-file content] (-> file asset/compile)
+          content (if (isa? String content) content (String. content "UTF-8"))]
+      (is (= expected (dump content)))
+      (is (= expected-file result-file)))))
+
+
+(defn test-syntax [root file expecteds]
+  (settings/with-options {:asset-roots [root]}
+    (try
+      (asset/compile file)
+      (is false)
+      (catch Exception e
+        (doseq [expected expecteds]
+          (is (has-text? (.toString e) expected)))))))
