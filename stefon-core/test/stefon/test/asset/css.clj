4706f6b0f076163ba7db81a9d1f050910c1fe242
Paul Biggar <paul.biggar@gmail.com>
2013-08-11 18:48:52 -0700

    Refactor css tests

--- stefon-core/test/stefon/test/asset/css.clj
+++ stefon-core/test/stefon/test/asset/css.clj
@@ -1,19 +1,14 @@
 (ns stefon.test.asset.css
-  (:require [clojure.java.io :as io]
-            [stefon.test.helpers :as h]
-            [stefon.asset :as asset]
-            [stefon.settings :as settings]
-            [stefon.asset.css :as css])
-  (:use clojure.test)
-  (:import stefon.asset.css.Css))
+  (:require [stefon.test.helpers :as h]
+            [stefon.util :refer (dump)])
+  (:use clojure.test))
 
-(deftest test-read-asset-css
-  (let [asset (asset/read-asset (Css.
-                                 (io/file "test/fixtures/assets/stylesheets/main.css")))]
-    (testing "adds a source comment"
-      (is (h/has-text? (:content asset) "/* Source: test/fixtures/assets/stylesheets/main.css */")))
-    (testing "includes file contents"
-      (is (h/has-text? (:content asset) "text-decoration: blink;")))))
+(deftest test-css
+  (h/test-expected "test/fixtures/assets"
+                   "stylesheets/main.css"
+                   "stylesheets/main.css"
+                   ".fancy {\n    text-decoration: blink;\n}"))
+
 
 ;; (deftest test-compress-css
 ;;   (let [uncompressed-css "   .content .p {\n color: #fff;\n }"
