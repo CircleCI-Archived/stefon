e8ff648e13c82a91a503cf4a0fbe0a3b072081cf
Paul Biggar <paul.biggar@gmail.com>
2013-08-13 17:31:58 -0700

    Finish data-uri tests

--- stefon-core/test/stefon/test/asset/reference.clj
+++ stefon-core/test/stefon/test/asset/reference.clj
@@ -15,8 +15,7 @@
   (h/test-expected "test/fixtures/reference/assets"
                    "javascripts/testpath.js.coffee.ref"
                    "/assets/javascripts/testpath-bcba95e202390838c2ef17d7b3885839.js"
-                   ["(function() {\n  var x;\n\n  x = '/assets/images/image-102c15cd1a2dfbe24b8a5f12f2671fc8.jpeg';\n\n}).call(this);\n"]
-                   :debug true))
+                   ["(function() {\n  var x;\n\n  x = '/assets/images/image-102c15cd1a2dfbe24b8a5f12f2671fc8.jpeg';\n\n}).call(this);\n"]))
 
 (deftest test-asset-uri-in-css
   (h/test-expected "test/fixtures/reference/assets"
@@ -35,11 +34,11 @@
 
 (deftest test-multiple-per-line
   (h/test-expected "test/fixtures/reference/assets"
-                   "javascripts/testmultiple.js.coffee.ref"
-                   "/assets/javascripts/testmultiple.js"
-                   ["var x = \"assets/image-somedigest.jpeg\";"]))
+                   "javascripts/testmultiple.js.ref"
+                   "/assets/javascripts/testmultiple-1fe91acb475290c2dbc2729abdc197ab.js"
+                   ["x = \"/assets/images/image-102c15cd1a2dfbe24b8a5f12f2671fc8.jpeg\"; y = \"/assets/images/image-102c15cd1a2dfbe24b8a5f12f2671fc8.jpeg\";"]))
 
 (deftest test-syntax-error
    (h/test-syntax "test/fixtures/reference/assets"
                   "javascripts/bad.js.coffee.ref"
-                  ["some unknown error"]))
+                  ["java.io.FileNotFoundException: Couldn't find some-file-which-doesnt-exit.jpeg in test/fixtures/reference/assets"]))
