5386e70cbb89cb12c4ba1f72ab48e779d6df7705
Paul Biggar <paul.biggar@gmail.com>
2013-08-14 18:02:02 -0700

    Simplify and fix lein-stefon-precompile

--- lein-stefon-precompile/src/leiningen/stefon_precompile.clj
+++ lein-stefon-precompile/src/leiningen/stefon_precompile.clj
@@ -3,33 +3,21 @@
   (:require [clojure.string :as string]
             [leiningen.core.eval :as eval]))
 
-(defn ns-resolve-string
+(defn split-ns
   "Given a string "
   [ns-string]
-  `(let [pair# (map symbol (string/split ~ns-string #"/"))
-         ns# (first pair#)
-         sym# (second pair#)]
-     (require ns#)
-     @(ns-resolve ns# sym#)))
-
-(defn resolve-stefon-options [opt]
-  `(let [opt# ~opt]
-     (cond
-      (map? opt#) opt#
-      (string? opt#) (let [val# ~(ns-resolve-string opt)]
-                       (cond
-                        (map? val#) val#
-                        (fn? val#) (val#))))))
+  (let [pair (map symbol (string/split ns-string #"/"))
+         ns (first pair)
+         sym (second pair)]
+     [ns sym]))
 
 (defn stefon-precompile
   [project]
-  (let [options (:stefon-options project)
-        options (->> project
-                     :stefon-options
-                     resolve-stefon-options)]
-    (println "options=" options)
+  (let [[ns sym] (-> project :stefon-options split-ns)]
     (eval/eval-in-project
      project
-     `(let []
-        (stefon.core/precompile ~options))
-     `(require 'stefon.core))))
+     `(let [options# (ns-resolve (quote ~ns) (quote ~sym))]
+        (println "options=" @options#)
+        (stefon.core/precompile @options#))
+     `(do (require 'stefon.core)
+          (require (quote ~ns))))))
