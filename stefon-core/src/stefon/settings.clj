af1370c5cba31ecb1ac561e3412b3758565475c9
Ryan McGowan <ryan@ryanmcg.com>
2013-09-07 18:28:47 -0700

    Add validation to settings.

--- stefon-core/src/stefon/settings.clj
+++ stefon-core/src/stefon/settings.clj
@@ -1,5 +1,6 @@
 (ns stefon.settings
   (:require [clojure.java.io :as io]
+            [clojure.string :as s]
             [stefon.util :refer (dump)]))
 
 (defonce ^:dynamic *settings*
@@ -9,8 +10,45 @@
    :manifest-file "resources/manifest.json" ;; you dont necesarily want this in the assets dir
    :precompiles [#".*"]}) ;; TODO: make this work
 
+(def ^:private validations
+  (let [sequence-of-strings? (fn [sequence-of-strings]
+                               (and (sequential? sequence-of-strings)
+                                    (every? string? sequence-of-strings)))
+        in-mode (fn [mode predicate]
+                  (fn [settings]
+                    (or (not= mode (:mode settings))
+                        (predicate settings))))]
+    [[map? "options must be a map"]
+     [(in-mode :production (comp string? :serving-root))
+      "when in production serving-root must be a string representing a path"]
+     [(comp string? :manifest-file)
+      "manifest-file must be a string representing a path"]
+     [(comp (partial contains? #{:production :development}) :mode)
+      "mode must be either"]
+     [(comp sequence-of-strings? :asset-roots)
+      "asset-roots must be a sequence of strings representing paths"]
+     [(in-mode :production (comp sequence-of-strings? :precompiles))
+      "when in production precompiles must be an sequence of strings "
+      "representing paths"]]))
+
+(defn validate
+  "Run validations on *settings* and throw an error when issues are found."
+  []
+  (let [errors (reduce (fn [errors [predicate & message]]
+                         (if (try
+                               ((complement predicate) *settings*)
+                               (catch Exception _ true))
+                           (conj errors (apply str message))
+                           errors))
+                       []
+                       validations)]
+    (when-not (empty? errors)
+      (throw (Exception. (str "Options (" *settings* ") are invalid: "
+                              (s/join ", " errors) \.))))))
+
 (defmacro with-options [options & body]
   `(binding [*settings* (merge *settings* ~options)]
+     (validate)
      (do ~@body)))
 
 (defn production? []
